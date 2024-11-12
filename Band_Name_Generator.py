62c329c82c67c16182b1b0653ba08209b743bbcd
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-11-12 12:27:26 -0500

    delete dup

--- Band_Name_Generator.py
+++
@@ -1,4 +0,0 @@
-print("Hi. let's build a band name generator!")
-city:str = input("What city did you grow up in?\n")
-pet_name:str = input("What is the name of your pet?\n")
-print("A good band name could be " + city + " " + pet_name+ "!")
