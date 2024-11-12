512e43ecdf18515d4aca0c6458d61415015463d5
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-11-12 12:05:48 -0500

    project1

---
+++ Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Hi. let's build a band name generator!")
+city:str = input("What city did you grow up in?\n")
+pet_name:str = input("What is the name of your pet?\n")
+print("A good band name could be " + city + " " + pet_name+ "!")
