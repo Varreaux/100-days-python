3cf62e49130e3a92821c0ba7fbe942e0cd78d6d5
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-01-18 11:29:45 -0500

    Day6 completed

---
+++ Day6_Functions_Reeborg.py
@@ -0,0 +1,21 @@
+
+# FINAL SOLUTION TO SOLVE MAZE - THANKS CHIP! :)
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# counter = 0
+# while not at_goal():  
+#     while right_is_clear():
+#         turn_right()
+#         move()
+#         counter+=1
+#         if counter > 4 and front_is_clear():
+#             move()
+#     counter = 0
+#     if front_is_clear():
+#         move()
+#     else:
+#         turn_left()
