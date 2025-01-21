2be7ec709c6ad5ed0c904be616ea1f41607fbd66
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-01-21 06:39:02 -0500

    Day7 completed

---
+++ Day7_Hangman.py
@@ -0,0 +1,69 @@
+import random
+import Day7_additional_file_words
+import Day7_additional_file_art
+
+# TODO-1: - Update the word list to use the 'word_list' from Day7_additional_file_words.py
+word_list = Day7_additional_file_words.word_list
+lives = 6
+
+# TODO-3: - Import the logo from Day7_additional_file_art.py and print it at the start of the game.
+print(Day7_additional_file_art.logo)
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+letters = []
+
+while not game_over:
+
+    # TODO-6: - Update the code below to tell the user how many lives they have left.
+    print(f"****************************{lives} LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess not in letters:
+        letters.append(guess)
+    else:
+        print(f"You have already guess {guess}")
+        continue
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+    #  e.g. You guessed d, that's not in the word. You lose a life.
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+
+        if lives == 0:
+            game_over = True
+
+            # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.
+            print(f"***********************YOU LOSE -- The correct word was {chosen_word}**********************")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    # TODO-2: - Update the code below to use the stages List from the file hangman_art.py
+    print(Day7_additional_file_art.stages[lives])
